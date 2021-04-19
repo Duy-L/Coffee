8ac3574b01b318aaa2702c012024824e97f0b7af
Duy Le <banle@ucsc.edu>
2021-04-19 02:10:20 -0700

    fixed audio refs and added audio files

--- src/frontend/components/app.js
+++ src/frontend/components/app.js
@@ -4,6 +4,12 @@ import Spotify from './spotify/spotify.js';
 import SpotifyPlaylist from './spotify/playlist.js';
 import Audio from './audio/audio.js';
 import expresso from './audio/assets/sounds/expresso_machine.mp3';
+import preparing from './audio/assets/sounds/preparing_drinks.mp3';
+import busy from './audio/assets/sounds/busy_night.mp3';
+import cup from './audio/assets/sounds/coffee_cups.mp3';
+import rainy from './audio/assets/sounds/rainy_day.mp3';
+import friend from './audio/assets/sounds/plus_one.mp3';
+import patio from './audio/assets/sounds/patio_day.mp3';
 
 const App = () => (
     <div>
@@ -12,9 +18,27 @@ const App = () => (
         <Spotify/>
         <SpotifyPlaylist/>
         <h1>From audio</h1>
-        <Audio file={expresso}/>
-        <Audio file={expresso}/>
-        <Audio file={expresso}/>
+        <h4>Preparing Drinks
+            <Audio file={preparing}/>
+       </h4>
+       <h4>Coffee Cups
+           <Audio file={cup} />
+       </h4>
+       <h4>Plus One
+           <Audio file={friend} />
+       </h4>
+        <h4>Machinery
+            <Audio file={expresso}/>
+        </h4>
+        <h4>Rainy Day
+            <Audio file={rainy}/>
+        </h4>
+        <h4>Sunny Day
+            <Audio file={busy}/>
+        </h4>
+        <h4>Outside Patio
+            <Audio file={patio}/>
+        </h4>
     </div>
 )
 
--- src/frontend/components/audio/audio.js
+++ src/frontend/components/audio/audio.js
@@ -3,42 +3,48 @@ import React, {Component} from 'react';
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 import './assets/audio.css';
 
-
 class Audio extends Component{
     constructor (props){
         super();
         this.state = {
             currState: 0
         }
+        this.audio = React.createRef();
         this.setVolume = this.setVolume.bind(this);
         this.playSound = this.playSound.bind(this);
         this.pauseSound = this.pauseSound.bind(this);    
     }
 
     setVolume(myVolume){
-        var myMedia = document.getElementById("mp3");
-        myMedia.volume = myVolume;
+        // var myMedia = document.getElementById("mp3");
+        // myMedia.volume = myVolume / 100;
+        this.audio.current.volume = myVolume / 100;
+
     }
     playSound(){
-        var vid = document.getElementById("mp3");
-        vid.play();
-        vid.loop = true;
+        // var vid = document.getElementById("mp3");
+        // vid.play();
+        // vid.loop = true;
+        this.audio.current.play();
+        this.audio.current.loop = true;
         this.setState({currState: 1});
     }
     pauseSound(){
-        var vid = document.getElementById("mp3");
-        vid.pause();
+        // var vid = document.getElementById("mp3");
+        // vid.pause();
+        this.audio.current.pause();
         this.setState({currState: 0});
     }
 
     componentDidMount(){
-        // document.getElementById("volume").value =".75";
+        // document.getElementById("volume").value ="75";
+        this.audio.current.volume=".75";
     }
 
     render(){
         return (
             <div>
-                <audio id="mp3" >
+                <audio id="mp3" ref={this.audio} >
                     <source src={this.props.file} type="audio/mp3"/>
                 </audio>
                 {this.state.currState === 0 &&(
@@ -53,10 +59,9 @@ class Audio extends Component{
                 )}
                 <input id ="volume"
                     type="range"
-                    min={0}
-                    max={1}
-                    // value={.75}
-                    step={0.025}
+                    min="0"
+                    max="100"
+                    step="1"
                     onChange={event => {
                     this.setVolume(event.target.valueAsNumber)
                     }}
